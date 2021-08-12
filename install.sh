08fd5429594b7f6498609dfd9a516da86a52addc
shMorganson <59580484+shMorganson@users.noreply.github.com>
2021-08-12 15:15:45 -0400

    Updated the files.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 LOGGEDINUSER="$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')"
 HOMEFOLDER=/Users/$LOGGEDINUSER
 LOGFILE=/var/log/dotfileinstall.log
-BREWLOC=/opt/homebrew
+BREWLOC=/opt/homebrew/bin/brew
 SETUPDIR=/Users/$LOGGEDINUSER/dot-files/Setup/
 
 # Making the log file.
@@ -74,11 +74,17 @@ fi
 # Making sure you have what you need before we move along.
 /bin/echo "We are making sure we have what we need to setup everyting." >> $LOGFILE
 
-if [ -d $BREWLOC ]; then
+if [ -f $BREWLOC ]; then
     /bin/echo "Brew is installed!" >> $LOGFILE
     /bin/echo "Continuning the setup." >> $LOGFILE
-    /bin/echo "Installing VIM." >> $LOGFILE
-    brew install vim
+    /bin/echo "Installing VIM..." >> $LOGFILE
+    $BREWLOC install vim
+    /bin/echo "Installing Terraform..." >> $LOGFILE
+    $BREWLOC install terraform
+    /bin/echo "Installing Vim Plug..." >> $LOGFILE
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    /bin/echo "Installing the Vim plugins..." >> $LOGFILE
+
     /bin/echo "Moving files to where they need to be." >> $LOGFILE
     cp # Move files from git to proper location.
 else
