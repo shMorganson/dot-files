03e48e2afcbf44c524b24ade7095d59781b0befa
shMorganson <59580484+shMorganson@users.noreply.github.com>
2021-08-31 12:45:38 -0400

    Updated gitignore, added Brewfile, updated install.sh

---
+++ Brewfile
@@ -0,0 +1,17 @@
+# 'brew tap'
+tap "epk/epk"
+
+# 'brew install'
+brew "node"
+brew "nvim"
+brew "code-minimap"
+brew "terraform"
+
+# 'brew install --cask'
+cask "font-sf-mono-nerd-font"
+
+# 'mas install'
+# mas "1Password", id: 443987910
+
+# 'whalebrew install'
+# whalebrew "whalebrew/wget"
--- install.sh
+++ install.sh
@@ -18,69 +18,46 @@ touch $HOMEFOLDER/Library/Logs/dotfileinstall.log
 if [ -f $BREWLOC ]; then
     /bin/echo "Brew is installed!" >> $LOGFILE
     /bin/echo "Continuning the setup." >> $LOGFILE
-    /bin/echo "Installing Node.js..." >> $LOGFILE
-    /$BREWLOC install node
     /bin/echo "Installing Oh My ZSH..." >> $LOGFILE
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     sleep 3
-    /bin/echo "Installing nvim..." >> $LOGFILE
-    $BREWLOC install nvim
-    sleep 3
     /bin/echo "Installing Vim Plug..." >> $LOGFILE
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    sleep 3
-    /bin/echo "Installing code-minimap so the Minimap plugin works..." >> $LOGFILE
-    $BREWLOC  install code-minimap
-    sleep 3
-    /bin/echo "Installing San Francisco Nerd Font..."
-    $BREWLOC tap epk/epk
-    $BREWLOC install --cask font-sf-mono-nerd-font
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     sleep 3
     /bin/echo "Moving the init.vim to the config directory..." >> $LOGFILE
     cp /Users/$LOGGEDINUSER/dot-files/init.vim /Users/$LOGGEDINUSER/.config/nvim/
     /bin/echo "Running Vim Plug to install Vim Plugins..." >> $LOGFILE
     vim +'PlugInstall --sync' +qa
     sleep 10
-    /bin/echo "Installing Terraform..." >> $LOGFILE
-    $BREWLOC install terraform
-    sleep 3
     /bin/echo "Moving files to where they need to be." >> $LOGFILE
     cp /Users/$LOGGEDINUSER/dot-files/.zshrc /Users/$LOGGEDINUSER
+    /bin/echo "Installing Brew Bundle and reading the Brewfile..." >> $LOGFILE
+    $BREWLOC bundle
+    $BREWLOC bundle --file $HOMEFOLDER/dot-files/Brewfile
     exit 0
 else
     /bin/echo "We are trying to install Brew." >> $LOGFILE
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     sleep 3
+    /bin/echo "Brew is installed!" >> $LOGFILE
     /bin/echo "Continuning the setup." >> $LOGFILE
-    /bin/echo "Installing Node.js..." >> $LOGFILE
-    /$BREWLOC install node
     /bin/echo "Installing Oh My ZSH..." >> $LOGFILE
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     sleep 3
-    /bin/echo "Installing vim..." >> $LOGFILE
-    $BREWLOC install vim
-    sleep 3
     /bin/echo "Installing Vim Plug..." >> $LOGFILE
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    sleep 3
-    /bin/echo "Installing code-minimap so the Minimap plugin works..." >> $LOGFILE
-    $BREWLOC  install code-minimap
-    sleep 3
-    /bin/echo "Installing San Francisco Nerd Font..."
-    $BREWLOC tap epk/epk
-    $BREWLOC install --cask font-sf-mono-nerd-font
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     sleep 3
     /bin/echo "Moving the init.vim to the config directory..." >> $LOGFILE
     cp /Users/$LOGGEDINUSER/dot-files/init.vim /Users/$LOGGEDINUSER/.config/nvim/
     /bin/echo "Running Vim Plug to install Vim Plugins..." >> $LOGFILE
     vim +'PlugInstall --sync' +qa
     sleep 10
-    /bin/echo "Installing Terraform..." >> $LOGFILE
-    $BREWLOC install terraform
-    sleep 3
     /bin/echo "Moving files to where they need to be." >> $LOGFILE
     cp /Users/$LOGGEDINUSER/dot-files/.zshrc /Users/$LOGGEDINUSER
+    /bin/echo "Installing Brew Bundle and reading the Brewfile..." >> $LOGFILE
+    $BREWLOC bundle
+    $BREWLOC bundle --file $HOMEFOLDER/dot-files/Brewfile
     exit 0
 fi
