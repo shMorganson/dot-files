66214587f2b428ca6d0066764f5949890e1ca1e2
shMorganson <59580484+shMorganson@users.noreply.github.com>
2021-09-01 14:58:45 -0400

    Added the lua files

---
+++ lua/Nvim-Tree/lua.lua
@@ -0,0 +1,65 @@
+vim.cmd [[packadd nvim-tree.lua]]
+
+vim.o.termguicolors = true
+
+local g = vim.g
+
+g.nvim_tree_side = "left"
+g.nvim_tree_width = 25
+g.nvim_tree_auto_open = 0
+g.nvim_tree_follow = 1
+g.nvim_tree_follow_update_path = 1
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_highlight_opened_files = 1
+g.nvim_tree_root_folder_modifier = ':~'
+g.nvim_tree_tab_open = 1
+g.nvim_tree_auto_resize = 1
+g.nvim_tree_disable_netrw = 1
+g.nvim_tree_hijack_netrw = 1
+g.nvim_tree_add_trailing = 1
+g.nvim_tree_group_empty = 0
+g.nvim_tree_lsp_diagnostics = 0
+g.nvim_tree_disable_window_picker = 0
+g.nvim_tree_hijack_cursor = 1
+g.nvim_tree_icon_padding = ' '
+g.nvim_tree_symlink_arrow = ' >> '
+g.nvim_tree_update_cwd = 1
+g.nvim_tree_respect_buf_cwd = 1
+g.nvim_tree_refresh_wait = 1000
+
+g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1
+}
+
+g.nvim_tree_icons = {
+     default = "",
+     symlink = "",
+     git = {
+       unstaged = "✗",
+       staged = "✓",
+       unmerged = "",
+       renamed = "➜",
+       untracked = "★",
+       deleted = "",
+       ignored = "◌"
+     },
+     folder = {
+       arrow_open = "",
+       arrow_closed = "",
+       default = "",
+       open = "",
+       empty = "",
+       empty_open = "",
+       symlink = "",
+       symlink_open = "",
+     },
+
+}
+
+local get_lua_cb = function(cb_name)
+    return string.format(":lua require'nvim-tree'.on_keypress('%s')<CR>", cb_name)
+end
---
+++ lua/plugins.lua
@@ -0,0 +1,30 @@
+------------------- PACKER ----------------------------------
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'               -- Packer can manage itself
+  use 'wfxr/minimap.vim'                     -- Minimap
+  use 'lukas-reineke/indent-blankline.nvim'  -- Indent Line
+  use {
+      'hoob3rt/lualine.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  use 'tpope/vim-fugitive'                   -- Fugitive for Git
+  use 'mbbill/undotree'                      -- Undo Tree for visual undo history
+  use 'dense-analysis/ale'                   -- ALE for linting
+  use 'shaunsingh/nord.nvim'                 -- Nord theme for nvim
+  use 'godlygeek/tabular'                    -- Required for vim-markdown to work
+  use 'plasticboy/vim-markdown'              -- Markdown plugin
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'Shougo/deoplete.nvim'                 -- Used for autocomplete
+  use 'neovim/nvim-lspconfig'                -- Used for configuring the nvim LSP.
+  use {
+    'kyazdani42/nvim-tree.lua',              -- Nvim tree
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+  use {
+  'ojroques/nvim-lspfuzzy',
+    requires = {
+      {'junegunn/fzf'},
+      {'junegunn/fzf.vim'},  -- to enable preview (optional)
+    },
+  }
+end)
