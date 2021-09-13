0caf1c6d22ab205d517cfee221ae2f66c15a4b2d
shMorganson <59580484+shMorganson@users.noreply.github.com>
2021-09-13 12:53:01 -0400

    Add indentline lua config file

---
+++ indentline/lua.lua
@@ -0,0 +1,14 @@
+vim.cmd [[packadd indent-blankline.nvim]]
+
+local opt = vim.opt
+
+opt.listchars = {
+  space = "⋅",
+}
+
+require("indent_blankline").setup {
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_end_of_line = false,
+  buftype_exclude = {"terminal", "minimap"}
+}
