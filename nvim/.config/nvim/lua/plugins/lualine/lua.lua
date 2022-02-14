0755af8f613531b3c0a658380373fa8bf8f3c926
shMorganson <59580484+shMorganson@users.noreply.github.com>
2022-02-14 17:07:16 -0500

    Made changes to lualine

--- nvim/.config/nvim/lua/plugins/lualine/lua.lua
+++ nvim/.config/nvim/lua/plugins/lualine/lua.lua
@@ -4,34 +4,214 @@ if not lualine_status_ok then
   return
 end
 
-require'lualine'.setup {
+-- Color table for highlights
+-- stylua: ignore
+local colors = {
+  bg       = '#3B4252',
+  fg       = '#ECEFF4',
+  yellow   = '#EBCB8B',
+  cyan     = '#88C0D0',
+  darkblue = '#5E81AC',
+  green    = '#A3BE8C',
+  orange   = '#D08770',
+  violet   = '#B48EAD',
+  magenta  = '#BF616A',
+  blue     = '#81A1C1',
+  red      = '#BF616A',
+}
+
+local conditions = {
+  buffer_not_empty = function()
+    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+  end,
+  hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+  end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end,
+}
+
+-- Config
+local config = {
   options = {
-    icons_enabled = true,
-    theme = 'nord',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = { 'NvimTree' }
+    -- Disable sections and component separators
+    component_separators = '',
+    section_separators = '',
+    theme = {
+      -- We are going to use lualine_c an lualine_x as left and
+      -- right section. Both are highlighted by c theme .  So we
+      -- are just setting default looks o statusline
+      normal = { c = { fg = colors.fg, bg = colors.bg } },
+      inactive = { c = { fg = colors.fg, bg = colors.bg } },
+    },
+    disabled_filetypes = { 'NvimTree' },
   },
   sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {
-     { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
+    -- these are to remove the defaults
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
     lualine_y = {},
-    lualine_z = {}
- },
-  tabline = {},
-  extensions = {}
+    lualine_z = {},
+    -- These will be filled later
+    lualine_c = {},
+    lualine_x = {},
+  },
+  inactive_sections = {
+    -- these are to remove the defaults
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    lualine_c = {},
+    lualine_x = {},
+  },
 }
+
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+-- Inserts a component in lualine_x ot right section
+local function ins_right(component)
+  table.insert(config.sections.lualine_x, component)
+end
+
+ins_left {
+  function()
+    return '▊'
+  end,
+  color = { fg = colors.blue }, -- Sets highlighting of component
+  padding = { left = 0, right = 1 }, -- We don't need space before this
+}
+
+ins_left {
+  -- mode component
+  function()
+    -- auto change color according to neovims mode
+    local mode_color = {
+      n = colors.blue,
+      i = colors.green,
+      v = colors.violet,
+      [''] = colors.blue,
+      V = colors.blue,
+      c = colors.magenta,
+      no = colors.yellow,
+      s = colors.orange,
+      S = colors.orange,
+      [''] = colors.orange,
+      ic = colors.yellow,
+      R = colors.violet,
+      Rv = colors.violet,
+      cv = colors.yellow,
+      ce = colors.yellow,
+      r = colors.cyan,
+      rm = colors.cyan,
+      ['r?'] = colors.cyan,
+      ['!'] = colors.yellow,
+      t = colors.yellow,
+    }
+      vim.api.nvim_command('hi! LualineMode guifg=' .. mode_color[vim.fn.mode()] .. ' guibg=' .. colors.bg)
+      return ''
+  end,
+  color = 'LualineMode',
+  padding = { right = 1 },
+}
+
+ins_left(
+  {'mode', color='LualineMode' }
+)
+
+ins_left {
+  'branch',
+  icon = '',
+  color = { fg = colors.violet, gui = 'bold' },
+}
+
+ins_left {
+  'filename',
+  cond = conditions.buffer_not_empty,
+  color = { fg = colors.magenta, gui = 'bold' },
+}
+
+ins_left {
+  -- filesize component
+  'filesize',
+  cond = conditions.buffer_not_empty,
+}
+
+-- Add components to right sections
+ins_right {
+  'o:encoding', -- option component same as &encoding in viml
+  fmt = string.lower, -- I'm not sure why it's upper case either ;)
+  cond = conditions.hide_in_width,
+  color = { fg = colors.green},
+}
+
+ins_right {
+  'fileformat',
+  fmt = string.upper,
+  icons_enabled = true, -- I think icons are cool but Eviline doesn't have them. sigh
+  color = { fg = colors.fg, gui = 'bold' },
+}
+
+ins_right {
+  'filetype'
+}
+
+ins_right { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+
+ins_right {
+  'location'
+}
+
+ins_right {
+  function()
+    return '▊'
+  end,
+  color = { fg = colors.blue },
+  padding = { left = 1 },
+}
+
+-- Now don't forget to initialize lualine
+lualine.setup(config)
+
+-- Old Config
+--require'lualine'.setup {
+--  options = {
+--    icons_enabled = true,
+--    theme = 'nord',
+--    component_separators = { left = '', right = ''},
+--    section_separators = { left = '', right = ''},
+--    disabled_filetypes = { 'NvimTree' }
+--  },
+--  sections = {
+--    lualine_a = {
+--      { 'mode', separator = { left = '' }, right_padding = 0 },
+--    },
+--    lualine_b = {'branch'},
+--    lualine_c = {'filename'},
+--    lualine_x = {'encoding', 'fileformat', 'filetype'},
+--    lualine_y = {'progress'},
+--    lualine_z = {
+--     { 'location', separator = { right = '' }, left_padding = 0 },
+--    },
+--  },
+--  inactive_sections = {
+--    lualine_a = {},
+--    lualine_b = {},
+--    lualine_c = {'filename'},
+--    lualine_x = {'location'},
+--    lualine_y = {},
+--    lualine_z = {}
+-- },
+--  tabline = {},
+--  extensions = {}
+--}
+
+-- Color table for highlights
+-- stylua: ignore
+
