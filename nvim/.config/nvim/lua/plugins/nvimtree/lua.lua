98de4526c038ee57ce9e8200aceaa878bd997eff
shmorganson <justin@numo.com>
2022-04-06 09:59:39 -0400

    Changed nvimtree settings

--- nvim/.config/nvim/lua/plugins/nvimtree/lua.lua
+++ nvim/.config/nvim/lua/plugins/nvimtree/lua.lua
@@ -56,8 +56,8 @@ require'nvim-tree'.setup {
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
-  open_on_tab         = false,
+  auto_close          = true,
+  open_on_tab         = true,
   hijack_cursor       = true,
   update_cwd          = true,
   update_to_buf_dir   = {
