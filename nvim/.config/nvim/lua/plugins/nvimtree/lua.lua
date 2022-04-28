a32eccd85daa0b0f4b81dabd300d3101eb6845da
shmorganson <justin@numo.com>
2022-04-28 11:04:38 -0400

    Fixed the breaking changes to the nvimtree config

--- nvim/.config/nvim/lua/plugins/nvimtree/lua.lua
+++ nvim/.config/nvim/lua/plugins/nvimtree/lua.lua
@@ -56,11 +56,8 @@ require'nvim-tree'.setup {
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
   open_on_tab         = true,
   hijack_cursor       = true,
-    enable = true,
-    auto_open = true,
   --diagnostics = {
   --  enable = true,
   --  icons = {
@@ -93,7 +90,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {}
