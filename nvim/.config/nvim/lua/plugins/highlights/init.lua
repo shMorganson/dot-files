082212a3e5c95bb861969e44457d1f2e3867c591
shMorganson <justin@numo.com>
2023-03-29 14:33:23 -0400

    Mode changes to match the PaperColor theme to my liking

--- nvim/.config/nvim/lua/plugins/highlights/init.lua
+++ nvim/.config/nvim/lua/plugins/highlights/init.lua
@@ -8,15 +8,11 @@ vim.cmd [[colorscheme PaperColor]]
 
 -- EXAMPLE vim.api.nvim_set_hl (0,'NvimTreeRootFolder', {fg='#A6DA95'})
 
--- nvim UI
--- highlight (0, 'StatusLine', {ctermbg = 15, bg = '#2E3440'})
--- highlight (0, 'EndOfBuffer', {ctermfg = 15, bg = '#2E3440', fg = '#2E3440'})
-
--- nvim UI
-
 -- Alpha
 highlight (0, 'AlphaHeaderColor', {ctermfg = 31, fg = '#0087af'})
-highlight (0, 'AlphaFooterColor', {ctermfg = 70, fg = '#5faf00'})
+highlight (0, 'AlphaFooterColor', {ctermfg = 70, fg = '#5faf00', bold = true })
+
+-- nvim UI
 highlight (0, 'DiagnosticError', { ctermbg = 255, ctermfg = 124, fg = '#af0000', bg = '#EEEEEE' })
 highlight (0, 'DiagnosticWarn', { ctermbg = 255, ctermfg = 214, fg = '#ffaf00', bg = '#EEEEEE' })
 highlight (0, 'DiagnosticInfo', { ctermbg = 255, ctermfg = 31, fg = '#0087af', bg = '#EEEEEE' })
@@ -35,3 +31,36 @@ highlight (0, 'NvimTreeFolderIcon', { ctermfg = 214, fg = "#ffaf00" })
 highlight (0, 'GitSignsAdd', { ctermfg = 70, ctermbg = 255, fg = '#5faf00', bg = '#eeeeee' })
 highlight (0, 'GitSignsChange', { ctermfg = 214, ctermbg = 255, fg = '#ffaf00', bg = '#eeeeee' })
 highlight (0, 'GitSignsDelete', { ctermfg = 160, ctermbg = 255, fg = '#d70000', bg = '#eeeeee' })
+
+-- Notify
+highlight (0, 'NotifyINFOBorder', { ctermfg = 31, ctermbg = 255, fg = '#0087af', bg = '#eeeeee' })
+highlight (0, 'NotifyINFOIcon', { ctermfg = 31, ctermbg = 255, fg = '#0087af', bg = '#eeeeee' })
+highlight (0, 'NotifyINFOTitle', { bold = true })
+highlight (0, 'NotifyERRORBorder', { ctermfg = 160, ctermbg = 255, fg = '#d70000', bg = '#eeeeee' })
+highlight (0, 'NotifyERRORIcon', { ctermfg = 160, ctermbg = 255, fg = '#d70000', bg = '#eeeeee' })
+highlight (0, 'NotifyERRORTitle', { bold = true })
+highlight (0, 'NotifyWARNBorder', { ctermfg = 214, ctermbg = 255, fg = '#ffaf00', bg = '#eeeeee' })
+highlight (0, 'NotifyWARNIcon', { ctermfg = 214, ctermbg = 255, fg = '#ffaf00', bg = '#eeeeee' })
+highlight (0, 'NotifyWARNTitle', { bold = true })
+highlight (0, 'NotifyDEBUGBorder', { ctermfg = 125, ctermbg = 255, fg = '#af005f', bg = '#eeeeee' })
+highlight (0, 'NotifyDEBUGIcon', { ctermfg = 125, ctermbg = 255, fg = '#af005f', bg = '#eeeeee' })
+highlight (0, 'NotifyDEBUGTitle', { bold = true })
+highlight (0, 'NotifyTRACEBorder', { ctermfg = 37, ctermbg = 255, fg = '#00afaf', bg = '#eeeeee' })
+highlight (0, 'NotifyTRACEIcon', { ctermfg = 37, ctermbg = 255, fg = '#00afaf', bg = '#eeeeee' })
+highlight (0, 'NotifyTRACETitle', { bold = true })
+
+-- Telescope
+highlight (0, 'TelescopePromptTitle', { ctermfg = 24, bold = true, fg = '#005f87' })
+highlight (0, 'TelescopePromptBorder', { ctermfg = 24, ctermbg = 255, fg = '#005f87', bg = '#eeeeee' })
+-- highlight (0, 'TelescopePromptNormal', { ctermbg=15 })
+highlight (0, 'TelescopePromptPrefix', { ctermfg = 125, fg = '#af005f' })
+highlight (0, 'TelescopeResultsTitle', { ctermfg = 24, bold = true, fg = '#005f87' })
+highlight (0, 'TelescopeResultsBorder', { ctermfg = 24, ctermbg = 255, fg = '#005f87', bg = '#eeeeee' })
+-- highlight (0, 'TelescopeResultsNormal', { ctermbg=15 })
+highlight (0, 'TelescopePreviewTitle', { ctermfg = 70, bold = true, fg = '#5faf00' })
+highlight (0, 'TelescopePreviewBorder', { ctermfg = 70, ctermbg = 255, fg = '#5faf00', bg = '#eeeeee' })
+-- highlight (0, 'TelescopePreviewNormal', { ctermbg=15 })
+highlight (0, 'TelescopeSelection', { ctermbg = 252, bg = '#d0d0d0' })
+-- highlight (0, 'TelescopeNormal', { guifg=#ECEFF4, guibg=#363944 })
+highlight (0, 'TelescopeSelectionCaret', { ctermfg = 125, fg = '#af005f' })
+highlight (0, 'TelescopeMatching', { ctermfg = 125, fg = '#af005f', bold = true })
