66d108b087fb890c7717a433a9605c01f820c5b3
shMorganson <justin@numo.com>
2023-08-09 10:52:44 -0400

    Added Mason theme changes.

--- nvim/.config/nvim/lua/plugins/highlights/init.lua
+++ nvim/.config/nvim/lua/plugins/highlights/init.lua
@@ -62,4 +62,15 @@ highlight (0, 'TelescopeMatching', { ctermfg = 125, fg = '#af005f', bold = true 
 
 -- Mason
 highlight (0, 'MasonNormal', { ctermfg = 0, ctermbg= 15, bg = '#f7f7f7', fg= '#000000' })
--- highlight (0, )
+highlight (0, 'MasonLink', { ctermfg = 4, fg = '#1B556B' })
+highlight (0, 'MasonHeader', { ctermfg = 4, fg = '#1B556B', bold = true })
+highlight (0, 'MasonHeading', { ctermfg = 5, fg = '#862753' })
+highlight (0, 'MasonHighlight', { ctermfg = 10, fg = '#9CD936' })
+highlight (0, 'MasonMutedBlock', { ctermfg = 4, fg = '#1B556B' })
+highlight (0, 'MasonHighlightBlock', { ctermfg = 10 })
+-- highlight (0, 'MasonMutedBlockBold', { ctermfg = 4, fg = '#1B556B', bold = true })
+highlight (0, 'MasonHeaderSecondary', { ctermfg = 10 })
+highlight (0, 'MasonHighlightBlockBold', { ctermfg = 4, fg = '#1B556B', bold = true })
+highlight (0, 'MasonHighlightSecondary', { ctermfg = 10 })
+highlight (0, 'MasonHighlightBlockSecondary', { ctermfg = 10 })
+-- MasonHighlightBlockBoldSecondary
