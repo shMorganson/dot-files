c63eb2954cddb7ef82e2a6d06402ea25efc73d81
Justin Sidick <justin@numo.com>
2025-02-20 10:52:23 -0500

    Added more plugins

---
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,28 @@
+-- Plugin to finish pairs.
+-- https://github.com/windwp/nvim-autopairs
+
+return {
+  'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+      opts = {
+        check_ts = true,
+        ts_config = {
+          lua = { "string", "source" },
+          javascript = { "string", "template_string" },
+          java = false,
+        },
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        fast_wrap = {
+          map = "<M-e>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+          offset = 0, -- Offset from pattern match
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "Search",
+          highlight_grey = "LineNr",
+        },
+      }
+}
